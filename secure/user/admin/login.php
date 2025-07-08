13a11ee16e8fb3067e3270e60dd69b7f0add7e5d
RehanZacky <rehanzacky.rz@gmail.com>
2025-07-08 21:25:39 +0700

    file changes

--- secure/user/admin/login.php
+++ secure/user/admin/login.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
         $_SESSION['role'] = $user['role'];
-        header("Location: ../../../admin/dashboard.php");
+        header("Location: ../../../admin/dashboard_admin.php");
         exit;
     } else {
         $error = "Username atau password salah!";
