c660f7424cbd16c1afdccae560e024864eefa5a3
FerdieArya <ferdiarya13@gmail.com>
2025-08-22 10:29:01 +0700

    footer changes

--- berita.php
+++ berita.php
@@ -263,7 +263,8 @@ $berita_result = mysqli_query($koneksi, $query_berita);
             </div>
         </div>
     </div>
-</footer> 
+</footer>
+ 
 <script>
     document.getElementById("menu-toggle").addEventListener("click", function () {
         const menu = document.getElementById("mobile-menu");
